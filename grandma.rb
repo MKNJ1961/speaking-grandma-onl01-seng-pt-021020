e65bcf4e14af1264438e3df3a64c92bdc4e214dd
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-13 01:04:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,13 @@
 #   end
 # end
 
-
+def speak_to_grandma(string)
+  if string != string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string.upcase
+    return "NO, NOT SINCE 1938!"
+  end
+end
 
 
     
