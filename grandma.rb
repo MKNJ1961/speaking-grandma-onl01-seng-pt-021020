abeacab565ad8bcdb39516867a03f5b56fd3df5e
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-13 01:04:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,20 +1,20 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+# def speak_to_grandma(phrase)
+#   if phrase == "I LOVE YOU GRANDMA!"
+#     return "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+#   elsif phrase == phrase.downcase
+#     return "HUH?! SPEAK UP, SONNY!"
     
-  elsif phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+#   elsif phrase == phrase.downcase
+#     return "HUH?! SPEAK UP, SONNY!"
     
-  elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else 
-    return "NO, NOT SINCE 1938!"
-  end
-end
+#   elsif phrase == phrase.upcase
+#     return "NO, NOT SINCE 1938!"
+#   else 
+#     return "NO, NOT SINCE 1938!"
+#   end
+# end
 
 
     
